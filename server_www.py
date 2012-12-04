ab5cc17c237b46868d6be3ae7e08333b9bd3d577
Alessandro Lorenzi <alessandro.lorenzi@gmail.com>
2012-12-04 23:39:25 +0100

    server www

---
+++ server_www.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+import sys
+import json
+
+
+
+class Arduino():
+	'''
+	Possono esserci fino a 10 arduini attaccati (fino a 100 luci)
+	La classe viene inizializzata con una tupla di porte seriali, una per ogni arduino.
+
+	Gli arduini vierranno gestiti come un unico arduino.
+	
+	se si riceve un messaggio tipo "on1-100" invia all'arduino giusto un numero da 1-100
+	se si riceve un messaggio tipo "off1-100" invia all'arduino giusto un numero da 101-100
+	
+	L'arduino deve ricevere sulla porta seriale una stringa, trasformarla in intero e accendere/spegnere il led giusto.
+	'''
+
+	def __init__(self, ports):
+		import serial
+		self.s=list()
+		for port in ports:
+			self.s.append( serial.Serial(port=port, baudrate=9600))
+
+	
+	
+	def decode(self, message):
+		import re
+		onstring="on[0-9]+"
+		offstring="off[0-9]+"
+		
+		onmatches = re.findall(onstring,message)
+		for match in onmatches:
+			self.sendon(match.replace("on",""))
+
+		offmatches = re.findall(offstring,message)
+		for match in offmatches:
+			self.sendoff(match.replace("off",""))
+		
+
+	def sendon(self, message):
+		from time import sleep
+		print("siamo in ON")	
+		print(str(int(message)-(int(message)/10*10) ))
+		self.s[int(message)/10].write(str(int(message)-(int(message)/10*10) ))
+		sleep(0.2)
+		
+
+	def sendoff(self, message):
+		from time import sleep
+		print("siamo in OFF")	
+		print(str(int(message)-(int(message)/10*10)+100))
+		self.s[int(message)/10].write(str(int(message)-(int(message)/10*10)+100))
+		sleep(0.2)
+		
+
+
+from time import sleep
+import httplib
+
+a=Arduino(['/dev/ttyACM0', '/dev/ttyACM1'])
+
+while (True):
+	
+	try:
+		conn = httplib.HTTPConnection('whitelabox.altervista.org')
+		conn.request("GET", "/accensioneluci.txt")
+		for command in conn.getresponse().read().split("\n"):
+			a.decode(command)
+		#print conn.getresponse().read()
+		conn.close()
+	except:
+		print("error")
+
+	sleep(2)
+
