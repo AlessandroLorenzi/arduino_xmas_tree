554e6ab29a9b5a59cefd05513b39e0fb29d10b71
Alessandro Lorenzi <alessandro.lorenzi@gmail.com>
2012-12-11 23:29:53 +0100

    Programmato il mini ecoalbero (solo random)

---
+++ miniecoalbero.ino
@@ -0,0 +1,24 @@
+void setup(){
+      pinMode( 2, OUTPUT);
+      pinMode( 3, OUTPUT);
+      pinMode( 4, OUTPUT);
+      pinMode( 5, OUTPUT);
+      pinMode( 6, OUTPUT);
+      pinMode( 7, OUTPUT);
+      pinMode( 8, OUTPUT);
+      pinMode( 9, OUTPUT);
+      pinMode(10, OUTPUT);
+      pinMode(11, OUTPUT);
+}
+
+void loop(){
+  int n, a;
+  n = random(2,12);
+  a = random(0,2);
+  if (a==1){
+    digitalWrite(n,HIGH);
+  }else{
+    digitalWrite(n,LOW);
+  }
+  delay(10);
+}
